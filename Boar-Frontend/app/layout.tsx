235a089acad47032f8b79b4a6cba3889df750d71
Lugacity01 <yinkaabeebadesina@gmail.com>
2026-02-28 10:18:53 +0100

    Career page was built and connect with the endpoint, contact was connect with backend and application and job are also working

--- Boar-Frontend/app/layout.tsx
+++ Boar-Frontend/app/layout.tsx
@@ -1,7 +1,7 @@
 
 
 import type React from "react";
-import type { Metadata } from "next";
+
 import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import { Footer } from "@/components/footer";
@@ -10,28 +10,6 @@ import { Providers } from "@/components/Providers";
 import { Toaster } from "react-hot-toast";
 
 
-export const metadata: Metadata = {
-  title: "BOAR Agro - Export-Grade Cocoa & Oilseed Production",
-  description:
-    "Feeding The Future, Sustainably With Export-Grade Cocoa & Oilseed Production. From our farms in Nigeria to processors across Europe, Asia, and the UK.",
-  icons: {
-    icon: [
-      {
-        url: "/boar_logo.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/boar_logo.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/boar_logo.png",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
-  },
-};
 
 export default function RootLayout({
   children,
